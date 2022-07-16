0e347d959da96d05b4d3630dd4b0850d9408be02
Roman <graff049@gmail.com>
2022-07-16 16:26:17 +0300

    feature(display): display owners_phonenumber and owner_normalized_phone

--- property/admin.py
+++ property/admin.py
@@ -7,7 +7,10 @@ from .models import Flat, Complaint
 class FlatAdmin(admin.ModelAdmin):
     search_fields = ('town', 'address', 'owner')
     readonly_fields = ('created_at',)
-    list_display = ('address', 'price', 'is_building_new', 'construction_year', 'town')
+    list_display = (
+        'address', 'price', 'is_building_new', 'construction_year', 'town',
+        'owners_phonenumber', 'owner_normalized_phone',
+    )
     list_editable = ('is_building_new',)
     list_filter = ('is_building_new', 'rooms_number', 'has_balcony')
     raw_id_fields = ('liked_by',)
