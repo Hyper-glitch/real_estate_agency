3847decdd31aee97416b29bedfc64d7b53093972
Roman Smirnov <r.smirnov@okenit.ru>
2022-07-19 16:55:08 +0300

    feature(schema_migration): add a new schema migration with db search indexes

--- property/models.py
+++ property/models.py
@@ -15,36 +15,41 @@ class Flat(models.Model):
     created_at = models.DateTimeField(
         'Когда создано объявление',
         default=timezone.now,
-        db_index=True)
-
+        db_index=True,
+    )
     description = models.TextField('Текст объявления', blank=True)
     price = models.IntegerField('Цена квартиры', db_index=True)
-
     town = models.CharField(
         'Город, где находится квартира',
         max_length=50,
-        db_index=True)
+        db_index=True,
+    )
     town_district = models.CharField(
         'Район города, где находится квартира',
         max_length=50,
         blank=True,
-        help_text='Чертаново Южное')
+        help_text='Чертаново Южное',
+    )
     address = models.TextField(
         'Адрес квартиры',
-        help_text='ул. Подольских курсантов д.5 кв.4')
+        help_text='ул. Подольских курсантов д.5 кв.4',
+    )
     floor = models.CharField(
         'Этаж',
         max_length=3,
-        help_text='Первый этаж, последний этаж, пятый этаж')
+        help_text='Первый этаж, последний этаж, пятый этаж',
+    )
 
     rooms_number = models.IntegerField(
         'Количество комнат в квартире',
-        db_index=True)
+        db_index=True,
+    )
     living_area = models.IntegerField(
         'количество жилых кв.метров',
         null=True,
         blank=True,
-        db_index=True)
+        db_index=True,
+    )
 
     has_balcony = models.NullBooleanField('Наличие балкона', db_index=True)
     active = models.BooleanField('Активно-ли объявление', db_index=True)
@@ -52,9 +57,14 @@ class Flat(models.Model):
         'Год постройки здания',
         null=True,
         blank=True,
-        db_index=True)
-    is_building_new = models.BooleanField('Новостройка', choices=BUILDING_CHOICES, null=True, default=None)
-    liked_by = models.ManyToManyField(User, related_name='liked_flats', blank=True, verbose_name='Кто поставил лайк')
+        db_index=True,
+    )
+    is_building_new = models.BooleanField(
+        'Новостройка', choices=BUILDING_CHOICES, null=True, default=None, db_index=True,
+    )
+    liked_by = models.ManyToManyField(
+        User, related_name='liked_flats', blank=True, verbose_name='Кто поставил лайк', db_index=True,
+    )
 
     class Meta:
         verbose_name = "Квартира"
@@ -85,10 +95,10 @@ class Complaint(models.Model):
 
 class Owner(models.Model):
     """Model that describes owner of flat(s)."""
-    full_name = models.CharField('ФИО владельца', max_length=200)
-    owners_phonenumber = models.CharField('Номер владельца', max_length=20)
-    owner_normalized_phone = PhoneNumberField(blank=True, verbose_name='Нормализованный номер владельца')
-    flats = models.ManyToManyField(Flat, related_name='owners', verbose_name='Квартиры в собственности')
+    full_name = models.CharField('ФИО владельца', max_length=200, db_index=True)
+    owners_phonenumber = models.CharField('Номер владельца', max_length=20, db_index=True)
+    owner_normalized_phone = PhoneNumberField(blank=True, verbose_name='Нормализованный номер владельца', db_index=True)
+    flats = models.ManyToManyField(Flat, related_name='owners', verbose_name='Квартиры в собственности', db_index=True)
 
     class Meta:
         verbose_name = "Собственник"
